60094518379b6ba9432d633ab0cc73fb446eb02e
Banana Shimakawa <shimakawa@c-fo.com>
2018-05-14 14:22:53 +0900

    goenv用にconfigを弄った

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,14 +2,13 @@ function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
 
+set -x GOPATH $HOME/go
+set -x PATH $HOME/.local/bin $HOME/.rbenv/shims $HOME/.goenv/bin $HOME/.pyenv/shims $GOPATH/bin $PATH
+
 rbenv init - | source
 pyenv init - | source
 status --is-interactive; and source (nodenv init -|psub)
 eval (direnv hook fish)
-
-set -x PATH $HOME/.local/bin $HOME/.rbenv/shims $HOME/.pyenv/shims $HOME/go/bin $PATH
-
-set -x GOPATH $HOME/go
-set -x GOROOT /usr/local/Cellar/go/1.9.2/libexec
+goenv rehash > /dev/null ^&1
 
 alias g='git'
