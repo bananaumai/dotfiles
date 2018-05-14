babc5fe3b6928cd95422396e444d93566b5ed166
Banana Shimakawa <shimakawa@c-fo.com>
2018-05-14 13:01:46 +0900

    config/fish更新

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,9 +4,12 @@ end
 
 rbenv init - | source
 pyenv init - | source
+status --is-interactive; and source (nodenv init -|psub)
 eval (direnv hook fish)
 
 set -x PATH $HOME/.local/bin $HOME/.rbenv/shims $HOME/.pyenv/shims $HOME/go/bin $PATH
 
 set -x GOPATH $HOME/go
-set -x GOROOT /usr/local/Cellar/go/1.8.1/libexec
+set -x GOROOT /usr/local/Cellar/go/1.9.2/libexec
+
+alias g='git'
